f70e57f62d575aac4f4b3779839de8130c506811
unknown <eoluwakayode34@gmail.com>
2020-03-24 15:40:34 +0100

    autocomplete search box integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './component/header/header.component';
+import Footer from './component/footer/footer.component';
+import {Route, Switch} from 'react-router-dom';
+import Homepage from './pages/homepage/homepage.component';
+import SignInSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignUpPage from './pages/sign-up-page/sign-up-page.component';
+import FindALecturer from './pages/find-a-lecturer/find-a-lecturer.compoent';
+import RateALecturer from './pages/rate-a-lecturer/rate-a-lecturer.component';
+import RateASchool from './pages/rate-a-school/rate-a-school.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+          <Switch>
+            <Route  exact path='/' component={Homepage}  />
+            <Route path='/signin' component={SignInSignUp} />
+            <Route path='/signup' component={SignUpPage} />
+            <Route path='/find-a-lecturer' component={FindALecturer} />
+            <Route path='/rate-a-lecturer' component={RateALecturer} />
+            <Route path='/rate-a-school' component={RateASchool} />
+
+          </Switch>
+        <Footer/>
     </div>
   );
 }
